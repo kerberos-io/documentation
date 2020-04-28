3a1fcee47ebea06593b3d3e24fae7c5bc6ff58b9
Cedric Verstraeten <cedric@verstraeten.io>
2020-04-28 09:09:30 +0200

    fix

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,28 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/.docz/.cache/dev-404-page.js"))),
+  "component---src-architectures-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/architectures.mdx"))),
+  "component---src-enterprise-get-started-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/enterprise/get-started.mdx"))),
+  "component---src-index-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/index.mdx"))),
+  "component---src-enterprise-installation-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/enterprise/installation.mdx"))),
+  "component---src-enterprise-releases-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/enterprise/releases.mdx"))),
+  "component---src-enterprise-index-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/enterprise/index.mdx"))),
+  "component---src-open-source-get-started-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/get-started.mdx"))),
+  "component---src-open-source-index-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/index.mdx"))),
+  "component---src-open-source-upgrade-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/upgrade.mdx"))),
+  "component---src-open-source-web-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/web.mdx"))),
+  "component---src-cloud-index-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/cloud/index.mdx"))),
+  "component---src-enterprise-license-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/enterprise/license.mdx"))),
+  "component---src-open-source-contribute-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/contribute.mdx"))),
+  "component---src-open-source-license-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/license.mdx"))),
+  "component---src-open-source-releases-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/releases.mdx"))),
+  "component---src-open-source-installation-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/installation.mdx"))),
+  "component---src-open-source-machinery-mdx": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/src/open-source/machinery.mdx"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/i353408/Vagrant/www/repos/documentation/.docz/src/pages/404.js")))
+}
+
